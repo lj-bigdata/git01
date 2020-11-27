f284c3f48cca9150351f42271286317049f36050
zhangsan <1457313929@qq.com>
2020-11-27 15:35:09 +0800

    idea

--- src/main/java/com/shenzhen/HelloWorld.java
+++ src/main/java/com/shenzhen/HelloWorld.java
@@ -3,5 +3,6 @@ package com.shenzhen;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("hello git");
     }
 }
